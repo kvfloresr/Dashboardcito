457ed2bbdb8a2403e732ea3e8bccce8084f052a2
kvfloresr <karenvfloresr@gmail.com>
2023-06-12 11:31:35 -0400

    ready

--- views/header.php
+++ views/header.php
@@ -3,9 +3,9 @@
 
 <div class="sidenav-header">
   <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-  <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
+  <a class="navbar-brand m-0" href="" target="_blank">
     <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-    <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
+    <span class="ms-1 font-weight-bold text-white">Dashboard</span>
   </a>
 </div>
 
@@ -66,73 +66,14 @@
       <i class="material-icons opacity-10">view_in_ar</i>
     </div>
   
-  <span class="nav-link-text ms-1">Virtual Reality</span>
+  <span class="nav-link-text ms-1">Proyectos</span>
 </a>
 </li>
 
 
-<li class="nav-item">
-<a class="nav-link text-white " href="./rtl.html">
-  
-    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-      <i class="material-icons opacity-10">format_textdirection_r_to_l</i>
-    </div>
-  
-  <span class="nav-link-text ms-1">RTL</span>
-</a>
-</li>
 
 
-<li class="nav-item">
-<a class="nav-link text-white " href="./notifications.html">
-  
-    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-      <i class="material-icons opacity-10">notifications</i>
-    </div>
-  
-  <span class="nav-link-text ms-1">Notifications</span>
-</a>
-</li>
 
-
-  <li class="nav-item mt-3">
-    <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
-  </li>
-
-<li class="nav-item">
-<a class="nav-link text-white " href="./profile.html">
-  
-    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-      <i class="material-icons opacity-10">person</i>
-    </div>
-  
-  <span class="nav-link-text ms-1">Profile</span>
-</a>
-</li>
-
-
-<li class="nav-item">
-<a class="nav-link text-white " href="./sign-in.html">
-  
-    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-      <i class="material-icons opacity-10">login</i>
-    </div>
-  
-  <span class="nav-link-text ms-1">Sign In</span>
-</a>
-</li>
-
-
-<li class="nav-item">
-<a class="nav-link text-white " href="./sign-up.html">
-  
-    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-      <i class="material-icons opacity-10">assignment</i>
-    </div>
-  
-  <span class="nav-link-text ms-1">Sign Up</span>
-</a>
-</li>
 
 
 
@@ -145,8 +86,8 @@
 
 <div class="sidenav-footer position-absolute w-100 bottom-0 ">
   <div class="mx-3">
-    <a class="btn btn-outline-primary mt-4 w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard?ref=sidebarfree" type="button">Documentation</a>
-    <a class="btn bg-gradient-primary w-100" href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
+    <a class="btn btn-outline-primary mt-4 w-100" href="" type="button">Actualizar</a>
+    <a class="btn bg-gradient-primary w-100" href="" type="button">Upgrade</a>
   </div>
   
 </div>
