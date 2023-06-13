80b4a43d0c6aaa5ca9802d727cce6bb0ebbc3984
kvfloresr <karenvfloresr@gmail.com>
2023-06-13 12:27:59 -0400

    aaa

--- views/header.php
+++ views/header.php
@@ -71,7 +71,16 @@
 </li>
 
 
-
+<li class="nav-item">
+<a class="nav-link text-white " href="producto">
+  
+    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+      <i class="material-icons opacity-10">user</i>
+    </div>
+  
+  <span class="nav-link-text ms-1">Usuario</span>
+</a>
+</li>
 
 
 
