d1125c55f7d75dd561979b7ff779f1b3488ca7dd
kvfloresr <karenvfloresr@gmail.com>
2023-06-13 12:09:09 -0400

    listo

---
+++ controllers/usuario.php
@@ -0,0 +1,13 @@
+<?php
+
+class Producto extends Controller{
+    function __construct()
+    {
+        // Llama al constructor de la clase padre
+        parent::__construct();
+        $this->view->render("usuario/index");
+      //  echo"Llamaste al controlador producto";
+    }
+}
+
+?>
